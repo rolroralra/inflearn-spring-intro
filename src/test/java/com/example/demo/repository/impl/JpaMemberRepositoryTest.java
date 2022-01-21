7d8ede8bfff56d5fb5783a3bbe52444398349e33
Shinyoung Kim <rolroralra@gmail.com>
2022-01-21 22:07:31 +0900

    feat: implement JPA with using Spring-Data-Jpa (JpaRepository extends PagingAndSortingRepository extends CrudRepository extends Repository)

--- src/test/java/com/example/demo/repository/impl/JpaMemberRepositoryTest.java
+++ src/test/java/com/example/demo/repository/impl/JpaMemberRepositoryTest.java
@@ -13,7 +13,6 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @Sql({"classpath:schema.sql"})
--- src/test/java/com/example/demo/service/MemberServiceIntegrationTest.java
+++ src/test/java/com/example/demo/service/MemberServiceIntegrationTest.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.UUID;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
@@ -22,7 +24,7 @@ public class MemberServiceIntegrationTest {
     public void test_회원가입() {
         // Given
         Member member = new Member();
-        member.setName("rolroralra");
+        member.setName("rolroralra" + UUID.randomUUID());
 
         // When
         Long memberId = memberService.join(member);
@@ -37,11 +39,12 @@ public class MemberServiceIntegrationTest {
     @Test
     public void test_중복회원가입_예외() {
         // Given
+        String name = "rolroralra" + UUID.randomUUID();
         Member member = new Member();
-        member.setName("rolroralra");
+        member.setName(name);
 
         Member member2 = new Member();
-        member2.setName("rolroralra");
+        member2.setName(name);
 
         // When
         memberService.join(member);
